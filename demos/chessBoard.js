4f2bc500305f778b6351e22e4d1b16d840b7d7d0
Neyla Rojas <neylarojas.developer@gmail.com>
2022-12-21 01:08:00 -0500

    - Add docs folder and practise exercise

--- demos/chessBoard.js
+++ demos/chessBoard.js
@@ -3,7 +3,7 @@ for(let i=0; i < 8; i++) {
     for(let j=0; j < 8; j++) {
 
         // If row is even
-        if( i % 2 == 0 ) {
+        if(i % 2 == 0) {
             // If colunm is even, add black
             if( j % 2 == 0 ) {
                 row += 'N '
@@ -21,24 +21,6 @@ for(let i=0; i < 8; i++) {
                 // if column is odd, add black
                 row += 'N '
             }
-        }
-    }
-    console.log(row)
-}
-
-// Esto no funciona
-for(let i=0; i < 8; i++) {
-    let row = ''
-    for(let j=0; j < 8; j++) {
-        switch (i, j) {
-            case (i % 2 == 0, j % 2 == 0):
-                row += 'N '
-            case (i % 2 == 0, j % 2 == 1):
-                row += 'B '
-            case (i % 2 == 1, j % 2 == 0):
-                row += 'B '
-            case (i % 2 == 1, j % 2 == 1):
-                row += 'N '
         }
     }
     console.log(row)
